c07ef7454a8e3fb3f90c40aeef400b866deb3876
Sergei <lpzreq@gmail.com>
2020-05-23 13:17:13 +0300

    fix

--- aws_scripts/install.sh
+++ aws_scripts/install.sh
@@ -2,5 +2,8 @@ uname -r
 id -u
 export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+pwd
+cd /home/ubuntu/app
+pwd
 /home/ubuntu/.nvm/versions/node/v14.3.0/bin/npm
-npm install
+cd /home/ubuntu/app && pwd && npm install
